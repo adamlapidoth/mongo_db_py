e4110ac59e13850b33bb18de5a9fd28a0f478da2
Adam Lapidoth <adamlapidoth@gmail.com>
2023-12-28 10:33:47 +0200

    created insert_to_mongodb

---
+++ src/task1.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+
+from video_game_character import Player
+
+my_client = MongoClient(host="localhost", port=27017)
+
+player1 = Player(name="player1", max_health=50, max_energy=25)
+
+
+def insert_to_mongodb(client: MongoClient, player: Player):
+    game_db = client["game"]
+    game_db.create_collection(name="players")
+    players_col = game_db["players"]
+    players_col.insert_one(player.stat())
+
+
+if __name__ == '__main__':
+    insert_to_mongodb(client=my_client, player=player1)
