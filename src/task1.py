9f015ee63595d603d9af74a081ce6fd30230d0b0
Adam Lapidoth <adamlapidoth@gmail.com>
2023-12-30 16:14:36 +0200

    changed insert_to_mongodb like in lecture

--- src/task1.py
+++ src/task1.py
@@ -1,3 +1,5 @@
+import copy
+
 from pymongo import MongoClient
 from pymongo.database import Database
 
@@ -10,14 +12,19 @@ player1 = Player(name="player1", max_health=50, max_energy=25)
 player2 = Player(name="player2", max_health=200, max_energy=300, items=[potion])
 
 
-def insert_to_mongodb(client: MongoClient, player: Player):
+def insert_to_mongodb(client: MongoClient, player: Player) -> int:
     game_db = client["game"]
     game_collections = game_db.list_collection_names()
     if "players" not in game_collections:
         game_db.create_collection(name="players")
     players_col = game_db["players"]
-    if not find_player_by_name(game_db, player.name):
-        players_col.insert_one(player.stat())
+    duplicate = find_player_by_name(game_db, player.name)
+    if duplicate:
+        return duplicate["_id"]
+    player_dict = copy.deepcopy(player.stat())
+    items_list = [item.stat() for item in player.items]
+    player_dict["items"] = items_list
+    return players_col.insert_one(player_dict).inserted_id
 
 
 def find_player_by_name(
@@ -43,9 +50,11 @@ def load_player(db: Database, player_name: str) -> Player:
 
 
 if __name__ == "__main__":
-    insert_to_mongodb(client=my_client, player=player1)
+    p1_id = insert_to_mongodb(client=my_client, player=player1)
+    print(f"{player1.name} ID: {p1_id}")
     player1_loaded = load_player(my_client.game, player_name="player1")
     assert player1_loaded == player1
-    insert_to_mongodb(client=my_client, player=player2)
+    p2_id = insert_to_mongodb(client=my_client, player=player2)
+    print(f"{player2.name} ID: {p2_id}")
     player2_loaded = load_player(my_client.game, player_name="player2")
     assert player2_loaded == player2
